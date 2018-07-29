2a2983d0069c6a6c0c69934a5cba43bd8d6aaa41
ChinthakaGunawardana <38156680+ChinthakaGunawardana@users.noreply.github.com>
2018-07-30 00:23:31 +0800

    clear varibles
    clear

--- NodeJSRox/hello-world/src/app/courses.component.ts
+++ NodeJSRox/hello-world/src/app/courses.component.ts
@@ -24,11 +24,24 @@ import { CoursesService } from './courses.service';
     </div>  
     
     <input [value]="email" (keyup.enter)="email = $event.target.value; onKeyUp()" />
-    
+    <h3>banana</h3>
+    <input [(ngModel)]="email" (keyup.enter)="onKeyUp()" />
+   
+
+
+
+    <h3>pipe</h3>
+    {{course.title | uppercase | lowercase}}<br/>
+    {{course.students|number}}<br/>
+    {{course.rating | number:'1.1-1'}}<br/>
+    {{course.price | currency:'USD':true}}<br/>
+    {{course.releasDate | date:'shortDate'}}<br/>
     `
 })
 export class CoursesComponent{
-    email='entr email hear';
+    //data
+    email='enter email hear';
+    //logic
     onKeyUp(){
         console.log("ENTER was Pressed", this.email);        
     }
@@ -54,6 +67,12 @@ export class CoursesComponent{
     
     imageUrl = "https://www.w3schools.com/images/w3schools_green.jpg";
 
-
+    course ={
+        title:"The Complete Angular course",
+        rating: 4.9745,
+        students: 30123,
+        price: 190.95,
+        releasDate: new Date(2016, 3 ,1)
+    }
 
 }
